e8c121ba1dcd2ffa0f0ea0d84d1631758cf31c17
Lin Liu <spliun60@126.com>
2013-10-12 10:08:03 +0800

    add catalogue edit

--- src/book/src/Controller/Admin/CatalogueController.php
+++ src/book/src/Controller/Admin/CatalogueController.php
@@ -19,7 +19,27 @@ class CatalogueController extends ActionController
 {
     public function editAction()
     {   
-        $this->view()->setTemplate('catalogue-view');
+        $bookid = $this->params('bookid');
+        $data = '[{"label":"\u7b2c\u4e00\u7ae0","id":1,"pid":0,"depth":0},{"label":"\u7b2c\u4e00\u8282","id":2,"pid":1,"depth":1},{"label":"\u7b2c\u4e00\u5c0f\u8282","id":3,"pid":2,"depth":2},{"label":"\u7b2c\u4e8c\u7ae0","id":4,"pid":0,"depth":0},{"label":"\u7b2c\u4e09\u7ae0","id":5,"pid":0,"depth":0}]';
+        $pageList = json_decode($data);        
+        $this->view()->assign('bookid', $bookid);
+        $this->view()->assign('title', 'Book Catalogue Edit');
+        $this->view()->assign('pages', $pageList);
+        $this->view()->setTemplate('catalogue-edit');
+    }
+    
+    public function saveAction()
+    {
+        $status = 1;
+        $message = __('Catalogue data saved successfully.');
+
+        $bookid    = $this->request->getPost('bookid');
+        $catalogue = $this->request->getPost('catalogue');
+
+        return array(
+            'status'    => $status,
+            'message'   => $message,
+        );
     }
   
     public function editItemAction()
