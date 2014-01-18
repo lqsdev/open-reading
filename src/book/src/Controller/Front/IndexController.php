b80fda5ec86f5579ecc02810b61f80220aa9d8eb
Lin Liu <spliun60@126.com>
2014-01-18 21:21:32 +0800

    文章正文改为docbook xml

--- src/book/src/Controller/Front/IndexController.php
+++ src/book/src/Controller/Front/IndexController.php
@@ -25,6 +25,33 @@ class IndexController extends ActionController
                 'controller' => 'Book', 
                 'action'     => 'list'
             )
-        ); 
+        );
+    }
+    
+    public function resetpasswordAction()
+    {
+        $request = $this->getRequest();
+
+        // Make sure that we are running in a console and the user has not tricked our
+        // application into running this action from a public web server.
+        if (!$request instanceof ConsoleRequest){
+            throw new \RuntimeException('You can only use this action from a console!');
+        }
+
+        // Get user email from console and check if the user used --verbose or -v flag
+        $userEmail   = $request->getParam('userEmail');
+        $verbose     = $request->getParam('verbose');
+
+        // reset new password
+        $newPassword = Rand::getString(16);
+
+        //  Fetch the user and change his password, then email him ...
+        // [...]
+
+        if (!$verbose){
+            return "Done! $userEmail has received an email with his new password.\n";
+        }else{
+            return "Done! New password for user $userEmail is '$newPassword'. It has also been emailed to him. \n";
+        }
     }
 }
